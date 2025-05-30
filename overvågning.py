2499ee8c5de118b8197ae66eaeb9cd145afd3355
Rarslan33 <146880770+Rarslan33@users.noreply.github.com>
2025-05-30 11:47:52 +0200

    Update overvågning.py

--- overvågning.py
+++ overvågning.py
@@ -4,12 +4,10 @@ import ssd1306
 import time
 import network
 
-#OLED setup (adjust pins/size if needed)
 i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
 current_y = 0
-
 
 ssid = 'Galaxy A42 5G0C95'
 password = 'jhza3668'
@@ -27,14 +25,13 @@ print("IP address:", wlan.ifconfig()[0])
 def show_alert():
     global current_y
 
-    # Clear screen if we go past bottom (to prevent overflow)
     if current_y > 54:
         oled.fill(0)
         current_y = 0
 
     oled.text("⚠ FALL DETECTED!", 0, current_y)
     oled.show()
-    current_y += 10  # Move down for next message
+    current_y += 10 
 
 def clear_oled():
     oled.fill(0)
@@ -58,9 +55,6 @@ def start_server():
             print("⚠️ FALL DETECTED!")
             show_alert()
 
-            # Optional: clear after a delay
-            #time.sleep(10)
-            #clear_oled()
 
         cl.send("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\nReceived")
         cl.close()
